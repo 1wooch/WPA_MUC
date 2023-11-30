5b66c0fce98b2c8c424f51c12ca42c3316793d29
wwc <onechoo1106@gmail.com>
2023-11-30 23:28:29 +1000

    now it saving by date and display total too and not go below 0 (count)

--- src/MainPage/Mainpage.js
+++ src/MainPage/Mainpage.js
@@ -2,34 +2,57 @@ import { useState } from "react";
 import AddModal from "./AddModal";
 
 function Main() {
-    const [checkDate,setCheckDate] = useState({storedDate:"",count:0});
-    const [dateList, setDateList] = useState([]);
-
+    
     const [count, setCount] = useState([]);
 
 
     const addNewCOUNT=(countName)=>{
-      // const today = new Date();
-      // const year = today.getFullYear();
-      // const month = (today.getMonth() + 1).toString().padStart(2, '0');
-      // const day = today.getDate().toString().padStart(2, '0');
-      // const formattedDate = `${year}-${month}-${day}`;
-
+      const today = getDate();
       setCount((prevCounts) => [
         ...prevCounts,
-        { name: countName, count: 0, checkDate: dateList },
+        { name: countName, checkCount: [{"date":today,"count":1}] },  //checkcount => {date,count}
       ]);
     }
 
-    const AdjustCount=(adjustvalue,index)=>{  
-      for (let i = 0; i < count.length; i++) {
-        if (i === index) {
-          count[i].count += adjustvalue;
-          count[i].checkDate.push(checkDate);
-          setCount([...count]);
-        }
-      }
+    const getDate=()=>{
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = (today.getMonth() + 1).toString().padStart(2, '0');
+      const day = today.getDate().toString().padStart(2, '0');
+      return `${year}-${month}-${day}`;
     }
+
+    const AdjustCount = (adjustvalue, index) => {
+      setCount((prevCounts) => {
+        return prevCounts.map((countItem, i) => {
+          if (i === index) {
+            const today = getDate();
+    
+            // Check if the date exists, and update the count if it does
+            const updatedCheckCount = countItem.checkCount.map((entry) => {
+              if (entry.date === today) {
+                //make not below 0
+                if(entry.count + adjustvalue < 0){
+                  return { ...entry, count: 0 };
+                }
+                else{
+                  return { ...entry, count: entry.count + adjustvalue };
+                }
+              }
+              return entry;
+            });
+    
+            // If the date doesn't exist, add a new entry
+            if (!updatedCheckCount.some((entry) => entry.date === today)) {
+              updatedCheckCount.push({ date: today, count: 1 });
+            }
+    
+            return { ...countItem, checkCount: updatedCheckCount };
+          }
+          return countItem;
+        });
+      });
+    };
 
 
 
@@ -46,14 +69,20 @@ function Main() {
             <h1> Multiuse  Counter</h1>
             
             <div>
-            <ul>
-                {count.map((count, index) => (
-                  <li key={index}>
-                    {count.name}: {count.count}
-                    
-                  </li>
-                  //add + and - with delete and edit buttons maybe 3 dot symbol
-                ))}
+              <ul>
+                  {count.map((countItem, index) => (
+                    <li key={index}>
+                      <button onClick={() => AdjustCount(-1, index)}>-</button>
+                      {countItem.name}:
+                      {countItem.checkCount.map((entry, entryIndex) => (
+                        <span key={entryIndex}>
+                          Date: {entry.date}, Count: {entry.count}
+                        </span>
+                      ))}
+                       <span>Total Count: {countItem.checkCount.reduce((sum, entry) => sum + entry.count, 0)}</span>
+                      <button onClick={() => AdjustCount(1, index)}>+</button>
+                    </li>
+                  ))}
               </ul>
             </div>
             <div>
